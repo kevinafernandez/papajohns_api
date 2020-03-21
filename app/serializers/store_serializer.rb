f38219c63cae26e6af6c0c2b08ecd2fad31f3917
kevin <kevinafernandezm@gmail.com>
2020-03-21 18:03:48 -0300

    FastJSON | Add StoreSerializer

---
+++ app/serializers/store_serializer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: stores
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  address    :text
+#  email      :string           default("francisco.abalan@pjchile.com")
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
+# == Seriealizer for Store
+class StoreSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :name, :address, :email, :phone
+end
