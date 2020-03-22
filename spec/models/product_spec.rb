4ed2a8df2fe163e3b7c8736ae486e30196e86c6a
kevin <kevinafernandezm@gmail.com>
2020-03-22 11:05:21 -0300

    Rspec | Model: Add new testing for products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,4 +20,30 @@ RSpec.describe Product, type: :model do
   describe 'relationship' do
     it { should have_and_belong_to_many(:stores) }
   end
+
+  describe 'validations' do
+    let(:valid_attributes) do
+      { name: 'THE WORKS',
+        sku: 'T-WORKS',
+        price: 5500,
+        product_type: 'Pizza' }
+    end
+
+    let(:invalid_attributes) do
+      { name: nil,
+        sku: nil,
+        price: -1,
+        product_type: 'not_a_type' }
+    end
+
+    it 'should not be valid with invalid attributes' do
+      subject.attributes = invalid_attributes
+      expect(subject).to_not be_valid
+    end
+
+    it 'should be valid with valid attributes' do
+      subject.attributes = valid_attributes
+      expect(subject).to be_valid
+    end
+  end
 end
