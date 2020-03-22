8cb64dcc577e187d44f93904b55e64c107e7b0ae
kevin <kevinafernandezm@gmail.com>
2020-03-21 23:51:44 -0300

    Migration | Create Products table

---
+++ db/migrate/20200321211554_create_products.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :sku
+      t.string :product_type
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
