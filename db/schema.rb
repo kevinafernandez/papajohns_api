8574699ea210f3b86f00d3755b1c017c770b0ce1
kevin <kevinafernandezm@gmail.com>
2020-03-22 16:25:26 -0300

    Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,32 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_321_213_709) do
+ActiveRecord::Schema.define(version: 2020_03_22_155253) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'name'
-    t.string 'sku'
-    t.string 'product_type'
-    t.integer 'price'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "orders", force: :cascade do |t|
+    t.integer "total"
+    t.bigint "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["store_id"], name: "index_orders_on_store_id"
   end
 
-  create_table 'stores', force: :cascade do |t|
-    t.string 'name'
-    t.text 'address'
-    t.string 'email', default: 'francisco.abalan@pjchile.com'
-    t.string 'phone'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "orders_products", id: false, force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.index ["order_id"], name: "index_orders_products_on_order_id"
+    t.index ["product_id"], name: "index_orders_products_on_product_id"
   end
 
-  create_table 'stores_products', id: false, force: :cascade do |t|
-    t.bigint 'store_id'
-    t.bigint 'product_id'
-    t.index ['product_id'], name: 'index_stores_products_on_product_id'
-    t.index ['store_id'], name: 'index_stores_products_on_store_id'
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "sku"
+    t.string "product_type"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "products_stores", id: false, force: :cascade do |t|
+    t.bigint "store_id", null: false
+    t.bigint "product_id", null: false
+    t.index ["product_id"], name: "index_products_stores_on_product_id"
+    t.index ["store_id"], name: "index_products_stores_on_store_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "email", default: "francisco.abalan@pjchile.com"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "orders", "stores"
 end
