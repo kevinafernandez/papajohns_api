8f092436fd77168f46dcf57aa1ad99afc40c3ad6
kevin <kevinafernandezm@gmail.com>
2020-03-22 11:17:25 -0300

    Rspec | controller: Delete unnecesary comments

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -3,9 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ProductsController, type: :controller do
-  # This should return the minimal set of attributes required to create a valid
-  # Product. As you add validations to Product, be sure to
-  # adjust the attributes here as well.
+  # set valid && invalid attributes
   let(:valid_attributes) do
     { name: 'SUPER-PEPPERONI',
       sku: 'SUP-PEPP',
@@ -19,10 +17,6 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       price: -1,
       product_type: 'not_a_type' }
   end
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ProductsController. Be sure to keep this updated too.
 
   describe 'GET #index' do
     it 'returns a success response' do
